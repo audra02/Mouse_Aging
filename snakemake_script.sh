3f3de7bdc25fbd82af488467e148a05433ee8a5e
aust0075 <aust0075@login3.mif>
2025-04-02 12:47:41 +0300

    adding gitignore and snakemake_script

---
+++ snakemake_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -p main
+#SBATCH -n4
+
+source /scratch/lustre/home/aust0075/miniforge3/etc/profile.d/conda.sh
+conda activate snakemake
+export PATH=/scratch/lustrte/home/aust0075/miniforge3/bin:$PATH
+snakemake -p --use-conda --cores 4
