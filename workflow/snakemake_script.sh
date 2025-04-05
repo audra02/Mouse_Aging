7bbe7cdc3a33744944a3a5f98c751f491213005d
aust0075 <aust0075@login3.mif>
2025-04-05 18:44:33 +0300

    some changes

--- workflow/snakemake_script.sh
+++ workflow/snakemake_script.sh
@@ -5,5 +5,5 @@
 
 source /scratch/lustre/home/aust0075/miniforge3/etc/profile.d/conda.sh
 conda activate snakemake
-export PATH=/scratch/lustrte/home/aust0075/miniforge3/bin:$PATH
+export PATH=/scratch/lustre/home/aust0075/miniforge3/bin:$PATH
 snakemake -p --use-conda --cores 4
