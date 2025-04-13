a05116a2fda7081d089f6c52ca523db7d657bf68
SoiTrash <meda.klap@gmail.com>
2025-04-13 16:57:14 +0300

    Update snakemake_slurm.sh

--- snakemake_slurm.sh
+++ snakemake_slurm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=chipseq_pipeline
+#SBATCH --job-name=chipseq
 #SBATCH --partition=main
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
 #SBATCH --time=10:00:00
 #SBATCH --output=logs/slurm/%x_%j.out
 #SBATCH --error=logs/slurm/%x_%j.err
-#SBATCH --mem=8G
 
-# Load necessary modules and conda
+# Clean problematic directories
+rm -rf results/trimmed/ results/aligned/ logs/trim_galore/
+
 source /scratch/lustre/home/mekl0332/miniforge3/etc/profile.d/conda.sh
 conda activate snakemake_env
 export PATH=/scratch/lustre/home/mekl0332/miniforge3/bin:$PATH
@@ -16,7 +16,9 @@ export PATH=/scratch/lustre/home/mekl0332/miniforge3/bin:$PATH
 # Create log directory if it doesn't exist
 mkdir -p logs/slurm
 
-# Run Snakemake with SLURM profile
+export PATH=/scratch/lustre/home/aust0075/miniforge3/bin:$PATH
+# Main execution
+snakemake --unlock
 snakemake -p \
     --use-conda \
     --conda-frontend conda \
